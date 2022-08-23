3d3b9bfb964dd662d4255ddbfc9edaf727b51923
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-08-23 18:13:48 +0200

    #1 uses router to support version in URI

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,135 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import parse from 'html-react-parser'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
-import parse from 'html-react-parser'
 import { classNames } from './utils'
-import manifests from './manifests'
+import type { Manifest } from './store'
+import manifests from './store'
 
 const App = () => {
-    const [selectedManifest, setSelectedManifest] = useState(manifests[0])
+    const [manifest, setManifest] = useState<Manifest | undefined>(undefined)
+
+    const [selectedVersion, setSelectedVersion] = useState(manifests[0].version)
+
+    const navigate = useNavigate()
+
+    const params = useParams()
+
+    useEffect(() => {
+        const switched = manifests.find(m => m.version === params.version)
+        setManifest(switched)
+        if (switched) {
+            setSelectedVersion(switched.version)
+        }
+    }, [params.version])
+
+    const onSwitchVersion = (v: string) => {
+        navigate(`/${v}`)
+    }
 
     return (
         <div className="flex flex-col w-full justify-center">
-            <div className="flex w-full md:max-w-4xl mx-auto items-center justify-between py-4 border-b-4 border-stone-600">
-                <h1 className="text-4xl">
-                    Manifest eegg.foundation
-                </h1>
-                <Listbox value={selectedManifest} onChange={setSelectedManifest}>
-                    {({ open }) => (
-                        <>
-                            <div className="relative mt-1">
-                                <Listbox.Button className="text-black text-4xl border-black border-4 pl-3 pr-10 py-2 text-left cursor-default outline-none">
-                                    <span className="block truncate">{selectedManifest.version}</span>
-                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                        <SelectorIcon className="w-5 h-5 text-stone-900" aria-hidden="true" />
-                                    </span>
-                                </Listbox.Button>
+            <div className="flex flex-col w-full md:max-w-4xl mx-auto space-y-4 px-2">
+                {manifest ? (
+                    <>
+                        <div className="w-full flex flex-row items-center justify-between py-4 border-b-4 border-stone-400">
+                            <h1 className="text-4xl">
+                                Manifest eegg.foundation
+                            </h1>
+                            <Listbox value={selectedVersion} onChange={onSwitchVersion}>
+                                {({ open }) => (
+                                    <>
+                                        <div className="relative mt-1">
+                                            <Listbox.Button className="text-black text-2xl lg:text-4xl border-black border-4 pl-3 pr-10 py-2 text-left cursor-default outline-none">
+                                                <span className="block truncate">{selectedVersion}</span>
+                                                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                    <SelectorIcon className="w-5 h-5 text-stone-900" aria-hidden="true" />
+                                                </span>
+                                            </Listbox.Button>
 
-                                <Transition
-                                    show={open}
-                                    as={Fragment}
-                                    leave="transition ease-in duration-100"
-                                    leaveFrom="opacity-100"
-                                    leaveTo="opacity-0"
-                                >
-                                    <Listbox.Options className="absolute z-10 mt-1 w-full bg-white max-h-60 text-4xl overflow-auto border-black border-4 outline-none">
-                                        {manifests.map((manifest) => (
-                                            <Listbox.Option
-                                                key={manifest.version}
-                                                className={({active}) => classNames(
-                                                    active ? 'text-white bg-black' : 'text-stone-900',
-                                                    'cursor-default select-none relative py-2 pl-3 pr-9',
-                                                )}
-                                                value={manifest}
+                                            <Transition
+                                                show={open}
+                                                as={Fragment}
+                                                leave="transition ease-in duration-100"
+                                                leaveFrom="opacity-100"
+                                                leaveTo="opacity-0"
                                             >
-                                                {({selected, active}) => (
-                                                    <>
-                                                        <span className="block truncate">
-                                                            {manifest.version}
-                                                        </span>
-                                                        {selected && (
-                                                            <span
-                                                                className={classNames(
-                                                                    active ? 'text-white' : 'text-stone-900',
-                                                                    'absolute inset-y-0 right-0 flex items-center pr-4',
-                                                                )}
-                                                            >
-                                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                            </span>
-                                                        )}
-                                                    </>
-                                                )}
-                                            </Listbox.Option>
-                                        ))}
-                                    </Listbox.Options>
-                                </Transition>
-                            </div>
-                        </>
-                    )}
-                </Listbox>
-            </div>
+                                                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white max-h-60 text-2xl md:text-4xl overflow-auto border-black border-4 outline-none">
+                                                    {manifests.map((manifest) => (
+                                                        <Listbox.Option
+                                                            key={manifest.version}
+                                                            className={({active}) => classNames(
+                                                                active ? 'text-white bg-black' : 'text-stone-900',
+                                                                'cursor-default select-none relative py-2 pl-3 pr-9',
+                                                            )}
+                                                            value={manifest.version}
+                                                        >
+                                                            {({selected, active}) => (
+                                                                <>
+                                                                    <span className="block truncate">
+                                                                        {manifest.version}
+                                                                    </span>
+                                                                    {selected && (
+                                                                        <span
+                                                                            className={classNames(
+                                                                                active ? 'text-white' : 'text-stone-900',
+                                                                                'absolute inset-y-0 right-0 flex items-center pr-4',
+                                                                            )}
+                                                                        >
+                                                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                        </span>
+                                                                    )}
+                                                                </>
+                                                            )}
+                                                        </Listbox.Option>
+                                                    ))}
+                                                </Listbox.Options>
+                                            </Transition>
+                                        </div>
+                                    </>
+                                )}
+                            </Listbox>
+                        </div>
+                        <div className="w-full flex flex-col py-4 border-b-4 border-stone-400">
+                            <h2 className="text-stone-400 text-2xl">
+                                EEGG = ROR + NFT
+                            </h2>
+                            <p>
+                                &copy; 2022 <span className="font-bold">https://manifest.eegg.foundation</span><br/>
+                                <a href="//eegg.foundation" target="_blank">eegg.foundation</a>,
+                                <a href="//peperafaj.cz" target="_blank">peperafaj.cz</a>,<br/>
+                                <a href="https://etherscan.io/address/0xee54e45ec1cda5a4d9e837ff3b4310721140da30" target="_blank">
+                                    <span className="hidden md:flex">
+                                        0xee54e45ec1cda5a4d9e837ff3b4310721140da30
+                                    </span>
+                                    <span className="md:hidden">
+                                        ETH peněženka
+                                    </span>
+                                </a><br/>
+                                Datum vzniku idee: 2022-01-04
+                            </p>
+                        </div>
 
-            <article className="prose m-auto py-8 px-2 md:px-0">
-                <h2 className="text-stone-400">
-                    EEGG = ROR + NFT
-                </h2>
-                <p>
-                    &copy; 2022 <span className="font-bold">https://manifest.eegg.foundation</span><br/>
-                    <a href="//eegg.foundation" target="_blank">eegg.foundation</a>,
-                    <a href="//peperafaj.cz" target="_blank">peperafaj.cz</a>,<br/>
-                    <a href="https://etherscan.io/address/0xee54e45ec1cda5a4d9e837ff3b4310721140da30" target="_blank">
-                        <span className="hidden md:flex">
-                            0xee54e45ec1cda5a4d9e837ff3b4310721140da30
-                        </span>
-                        <span className="md:hidden">
-                            ETH peněženka
-                        </span>
-                    </a><br/>
-                    Datum vzniku idee: 2022-01-04
-                </p>
-                <div>{parse(selectedManifest.content)}</div>
-            </article>
+                        <article className="prose m-auto py-8 px-2 md:px-0">
+                            {parse(manifest.content)}
+                        </article>
+                    </>
+                ) : (
+                    <div className="flex min-h-screen justify-center">
+                        <div className="flex flex-col space-y-4 m-auto">
+                            <p className="text-center text-red-600">
+                                Verze <span className="font-bold">{params.version}</span> neexistuje.
+                            </p>
+                            <p className="text-center">
+                                <Link to={`/${manifests[0].version}`} className="text-2xl inline-flex bg-white text-black px-4 py-2 border-black border-4 font-bold hover:bg-black hover:text-white transition-colors ease-in-out duration-300">
+                                    Zkuste poslední {manifests[0].version}
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,16 @@
 import contentV10  from './manifests/v1.0.html?raw'
 import contentV11  from './manifests/v1.1.html?raw'
 
-const manifests = [
+export type Manifest = {
+    version: string
+    content: string
+}
+
+/**
+ * Note: manifests must be sorted descending to keep the functionality.
+ */
+
+const manifests = <Manifest[]>[
     {
         version: 'v1.1',
         content: contentV11,
